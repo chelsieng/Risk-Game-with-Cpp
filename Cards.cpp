0178baa15802638eb243812bc047c6be240e4179
Domi2000 <71415552+Domi2000@users.noreply.github.com>
2020-10-01 19:29:10 -0400

    Majority of Part 5
    Created the classes required (card, subclasses of card, deckNode, deck, hand). Deck was implemented as a linked list, and Hand was implemented as a vector. Assignment operator and copy constructors have yet to be implemented. Play() method requires implementation of Orders before it can actually create any orders (though it does everything else that it should). I have been testing everything with main so the driver file has yet to be changed.

--- Cards.cpp
+++ Cards.cpp
@@ -1,1 +1,171 @@
 #include "Cards.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+
+Card::Card()
+	: type(new string("default card")), description(new string("nothing here"))
+{
+}
+
+Card::Card(string t)
+	: type(new string(t)), description(new string("nothing here yet"))	//originally used &t which created many many
+{}																//errors, not sure why, think it has to do with improper initialization
+
+string Card::getType()
+{
+	return *type;	//returns the string itself, not the pointer
+}
+
+void Card::setDescription(string t)
+{	
+	*description = t;
+}
+
+string Card::getDescription() {
+	return *description;		//returns the string itself, not the pointer
+}
+
+void Card::printDescription() {
+	cout << this->getDescription() << endl;
+}
+
+void Card::play()
+{
+	//switch(*type)
+	//create different order based on type
+	cout << "You played a card of type: " << this->getType() << endl;
+	//return pointer to order created
+}
+
+BombCard::BombCard()
+	: Card("bomb")
+{
+	setDescription("This is a bomb card. More info will be added later");
+}
+
+DiplomacyCard::DiplomacyCard()
+	: Card("Diplomacy")
+{
+	setDescription("This is a diplomacy card. More info will be added later");
+}
+
+ReinforcementCard::ReinforcementCard()
+	: Card("Reinforcement")
+{
+	setDescription("This is a reinforcement card. More info will be added later");
+}
+
+AirliftCard::AirliftCard()
+	: Card("Airlift")
+{
+	setDescription("This is an airlift card. More info will be added later");
+}
+
+BlockadeCard::BlockadeCard()
+	: Card("Blockade")
+{
+	setDescription("This is a blockade card. More info will be added later");
+}
+
+
+
+//Deck and deckNode stuff:
+
+deckNode::deckNode()
+{
+}
+
+deckNode::deckNode(Card* thedata, deckNode* theLink)
+	: data(thedata), link(theLink)
+{
+}
+
+deckNode* const deckNode::getLink()
+{
+	return link;
+}
+
+Card* const deckNode::getData()
+{
+	return data;
+}
+
+void deckNode::setData(Card* theData)
+{
+	data = theData;
+}
+
+void deckNode::setLink(deckNode* theLink)
+{
+	link = theLink;
+}
+
+Deck::Deck(deckNodePtr thehead)
+	: head(thehead)
+{
+}
+
+void Deck::addToDeck(Card* theData)
+{
+	head = new deckNode(theData, head);
+}
+
+Card* Deck::draw()
+{
+	if (head == NULL) {
+		cout << "The deck is empty! Cannot draw a card." << endl;
+		return NULL;
+	}
+	else {
+		Card* topOfStack = head->getData();
+		head = head->getLink();
+		return topOfStack;
+	}
+}
+
+void Deck::placeOnBottom(Card* theData)
+{
+	if (head == NULL) {
+		addToDeck(theData);
+	}//end of if (deck empty)
+	else {
+		deckNodePtr position = head;
+		while (position->getLink() != NULL) {
+			position = position->getLink();
+		}
+		position->setLink(new deckNode(theData, NULL));
+	}//end of else (deck not empty)
+}
+
+
+
+Hand::Hand(int l, Deck* d)
+	: limit(new int(l)), gameDeck(d), cardsInHand()	//again, note to self, DO NOT USE &(parameter) to try
+{													//to initialize pointer: DOING SO DOES NOT WORK!!!
+											//Also beware, had we taken a Deck as a parameter instead and used new(), it wouldn't be pointing to the right object!
+	for (int i = 0; i < *limit; i++) {				//...Maybe. Verify this.
+		Card* c = d->draw();
+		cardsInHand.push_back(c);
+	}
+}
+
+void Hand::showCardsInHand()
+{
+	int size = cardsInHand.size();
+	for (int i = 0; i < size; i++) {
+		cout << i << ": " << cardsInHand.at(i)->getDescription() << endl;
+	}
+}
+
+
+
+void Hand::playCardAtIndex(int i)
+{
+	Card cardToPlay = *cardsInHand.at(i);
+	cardsInHand.at(i)->play();
+	gameDeck->placeOnBottom(cardsInHand.at(i));
+	cardsInHand.erase(cardsInHand.begin() + i); //the begin() part is necessary, it seems (can't just use index)
+	
+}
--- Cards.h
+++ Cards.h
@@ -1,11 +1,110 @@
 #pragma once
 #ifndef A1_CARDS_H
 #define A1_CARDS_H
+#include "player.h"
+#include "Orders.h"
+#include <string>
+#include <vector>
 
+using namespace std;
+//as per the instructions of the assignment, all data members (of user-made classes) must be of the pointer type
 
-class Cards {
+class Card {
 
+private:
+	string* type; //points to string whose value is either bomb, reinforcement, blockade, airlift, or diplomacy 
+	string* description; //points to string that contains description of card
+public:
+	Card(); //default constructor, do not use.
+	Card(string t);
+	string getType();
+	void setDescription(string t);
+	string getDescription();
+	void printDescription();
+	//unfortunately, we cannot declare or define the full Play() method here, since it requires a Deck object as
+	//input, but the Deck class is later in the file (a forward declaration will not work for a Parameter of
+	//	an incomplete type, to the best of my knowledge).
+	//So play() will simply create the order and add it to the list, NOT add it back to the deck.
+	//To do everything required, we will have another method in the Hand class to play() a specific card and then
+	// add it back to the game deck
+	void play();	//This should actually return an order (or a pointer to one) -> WAITING FOR ORDERS class to be implemented
+		//that is to say, won't remain void
+};//end of Card class
+
+//The specific types of cards are implemented as a series of subclasses (of the class Card):
+
+class BombCard : public Card {
+public:
+	BombCard();
+};//end of BombCard class
+
+class DiplomacyCard : public Card {
+public:
+	DiplomacyCard();
+};//end of DiplomacyCard class
+
+class ReinforcementCard : public Card {
+public:
+	ReinforcementCard();
+};//end of ReinforcementCard class
+
+class AirliftCard : public Card {
+public:
+	AirliftCard();
+};//end of AirliftCard class
+
+class BlockadeCard : public Card {
+public:
+	BlockadeCard();
+};//end of BlockadeCard class
+
+//Now we must create a deck class which will contain whatever cards we add to it
+//-> Let's make the deck a linked (or doubly linked if necessary) list
+
+class deckNode {
+public:
+	deckNode();
+	deckNode(Card* thedata, deckNode* theLink);
+
+	deckNode* const getLink();
+	Card* const getData();
+	void setData(Card* theData);
+	void setLink(deckNode* theLink);
+
+private:
+	Card* data;	//all data members must be of pointer type :(
+	deckNode* link;
+
+};//END of deckNode class
+typedef deckNode* deckNodePtr;
+
+class Deck {
+	//since the deck functions like a stack of cards, we don't have to create functions to add or remove cards to/from
+	//a specific index (we draw from the head, and put a card back in at the bottom)
+private:
+	deckNodePtr head;
+public:
+	//add default constructor even though we won't use it
+	Deck(deckNodePtr thehead);
+	void addToDeck(Card* theData); //(equivalent to adding to head)
+	Card* draw();	//(equivalent to removing from head)
+	void placeOnBottom(Card* theData);
 };
+//END of deck class
 
+//Finally, the hand class. In this case an array *seems* to make sense, but changing hand size may pose an issue...
+//No, we will definitely use vectors instead
+
+class Hand {
+private:
+	Deck* gameDeck;	//the deck the hand will draw from
+	int* limit;
+	std::vector<Card*> cardsInHand; //will be initialized in constructor
+public:
+	Hand(int l, Deck* d);
+	void showCardsInHand();
+	void playCardAtIndex(int i);
+	void addToHand(Card* c);
+};
 
 #endif //A1_CARDS_H
