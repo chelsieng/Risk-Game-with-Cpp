8565115d9ee29a22521bd506d5f838b863e2c941
Jacob Guirguis <jacob.guirguis@gmail.com>
2020-10-08 03:45:41 -0400

    .h and .cpp files nearly complete. Save State

--- Part 4/Player.cpp
+++ Part 4/Player.cpp
@@ -1,1 +1,99 @@
 #include "Player.h"
+#include "Cards.h"
+
+// default constructor
+Player::Player() :
+        playerHand{nullptr},
+        playerOrders(std::vector<Orders*>{nullptr}),
+        playerTerritories(std::vector<Territory*>{nullptr})
+{}
+
+// parameterized constructor
+Player::Player(Hand hand, std::vector<Orders> orders, std::vector<Territory> territories) : playerHand{&hand} {
+    for (auto o : orders) {
+        playerOrders.push_back(&o);
+    }
+
+    for (auto t : territories) {
+        playerTerritories.push_back(&t);
+    }
+}
+
+// copy constructor
+Player::Player(const Player &source) {
+
+    // HAND
+    // deallocate
+    delete this->playerHand;
+    this->playerHand = nullptr;
+
+    // copy using Hand copy constructor
+    this->playerHand = new Hand(*source.playerHand);
+
+    // ORDER
+    // deallocate
+    for (Orders* o : this->playerOrders) {
+        delete o;
+        o = nullptr;
+    }
+
+    // copy using Order copy constructor
+    for (int i = 0; i < source.playerOrders.size(); i++) {
+        this->playerOrders.push_back(new Orders(*source.playerOrders.at(i)));
+    }
+
+    // TERRITORIES
+    // deallocate
+    for (Territory* t : this->playerTerritories) {
+        delete t;
+        t = nullptr;
+    }
+
+    // copy using Territory copy constructor
+    for (int i = 0; i < source.playerTerritories.size(); i++) {
+        this->playerTerritories.push_back(new Territory(*source.playerTerritories.at(i)));
+    }
+}
+
+// destructor
+Player::~Player() {
+    // free hand
+    delete playerHand;
+    playerHand = nullptr;
+
+    // free orders
+    for (Orders* o : playerOrders) {
+        delete o;
+        o = nullptr;
+    }
+
+    // free territories
+    for (Territory* t : playerTerritories) {
+        delete t;
+        t = nullptr;
+    }
+}
+
+Player::Player(Hand hand, std::vector<Orders *> orders, std::vector<Territory *> territories) {
+
+}
+
+void Player::issueOrder(std::string orderType) {
+
+}
+
+std::vector<Territory *> Player::toDefend() {
+    return nullptr;
+}
+
+std::vector<Territory *> Player::toAttack() {
+    return nullptr;
+}
+
+Player &Player::operator=(const Player &player) {
+    return <#initializer#>;
+}
+
+std::ostream &operator<<(ostream &ostream, const Player &player) {
+    return <#initializer#>;
+}
--- Part 4/Player.h
+++ Part 4/Player.h
@@ -1,11 +1,27 @@
 #pragma once
-#ifndef A1_PLAYER_H
-#define A1_PLAYER_H
 
+#include "Cards.h"
+#include "Orders.h"
+#include "Map.h"
 
 class Player {
+private:
+    Hand* playerHand;
+    std::vector<Orders*> playerOrders;
+    std::vector<Territory*> playerTerritories{};
 
+public:
+    Player(); // default Constructor
+    Player(Hand hand, std::vector<Orders*> orders, std::vector<Territory*> territories); // parameterized Constructor
+    Player(const Player &player); // copy constructor
+    ~Player(); // destructor
+
+    void issueOrder(std::string orderType);
+    std::vector<Territory*>toDefend();
+    std::vector<Territory*>toAttack();
+
+    Player& operator = (const Player &player); // assignment operator
+
+    // stream insertion operator (free function)
+    friend std::ostream& operator << (std::ostream& ostream, const Player &player);
 };
-
-
-#endif //A1_PLAYER_H
