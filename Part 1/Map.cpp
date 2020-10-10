cfd7049f77f980474915c36474b1f2d52296dfdd
chelsieng <chelsiengmk@gmail.com>
2020-10-10 18:21:44 -0400

    Map class and Test case 1 done

--- Part 1/Map.cpp
+++ Part 1/Map.cpp
@@ -1,35 +1,64 @@
 #include "Map.h"
+#include "../Part 1/Graph.h"
 #include <vector>
 #include <string>
 #include <iostream>
 
 using namespace std;
+using namespace graph;
+int Army::Army::counter = 0;
 
-Map::Army::Army(Player *player) {
+//Constructor for armies of player
+Army::Army(Player *player) {
     owner = player;
     static int counter = 0;
     *id = ++counter;
 }
 
-int Map::Army::getId() const {
+
+Army::Army(const Army &army) {
+    owner = army.owner;
+    static int counter = 0;
+    *id = ++counter;
+} //End of copy constructor
+
+Army::~Army() {
+    delete owner;
+    delete id;
+    --counter;
+} //End of destructor
+
+Army &Army::operator=(const Army &army) {
+    if (this == &army) {
+        return *this;
+    }
+    delete owner;
+    owner = army.owner;
+    id = army.id;
+    return *this;
+} //End of assignment operator
+
+//get ID of army
+int Army::Army::getId() const {
     return *id;
 }
 
-void Map::Army::print() const {
-    cout << "Army " << *id << " is owned by player " << owner << endl;
-}
+ostream &operator<<(ostream &output, const Army &army) {
+    output << "Army " << army.id << " is owned by player " << army.owner << endl;
+    return output;
+} //End of insertion operator
 
-int Map::Territory::counter = 0;
+int Territory::Territory::counter = 0;
 
 // Constructor for a territory that is not the initial starting territory
-Map::Territory::Territory() {
+Territory::Territory::Territory() {
     *id = ++counter;
     owner = nullptr;
     armies = new vector<Army *>;
 }
 
 // Constructor for the territory that players start on
-Map::Territory::Territory(Player *player) {
+Territory::Territory(Player *player) {
     *id = ++counter;
     owner = player;
     armies = new vector<Army *>;
@@ -41,23 +70,282 @@ Map::Territory::Territory(Player *player) {
     }
 }
 
-int Map::Territory::getId() const {
+Territory::Territory(const Territory &territory) {
+    *id = ++counter;
+    owner = territory.owner;
+    armies = territory.armies;
+} //End of copy constructor
+
+Territory::~Territory() {
+    delete owner;
+    delete armies;
+    delete id;
+    --counter;
+} //End of destructor
+
+Territory &Territory::operator=(const Territory &territory) {
+    if (this == &territory) {
+        return *this;
+    }
+    delete owner;
+    delete armies;
+    owner = territory.owner;
+    armies = territory.armies;
+    id = territory.id;
+    return *this;
+} //End of assignment operator
+
+//get ID of territory
+int Territory::Territory::getId() const {
     return *id;
 }
 
-void Map::Territory::print() const {
-    if (armies->size() > 0) {
-        cout << "Army(ies) on territory " << this->getId() << " :" << endl;
-        for (Army *army : *armies) {
-            army->print();
+ostream &operator<<(ostream &output, const Territory &territory) {
+    if (!territory.armies->empty()) {
+        output << "Army(ies) on territory " << territory.getId() << " :" << endl;
+        for (Army *army : *territory.armies) {
+            cout << army;
         }
-        cout << "The territory is owned by ";
-        cout << owner;
-        cout << endl;
+        output << "The territory is owned by " << territory.owner << endl;
     } else {
-        cout << "Territory " << this->getId() << " is owned by no one" << endl;
+        output << "Territory " << territory.getId() << " is owned by no one" << endl;
     }
+    return output;
+} //End of insertion operator
 
+int Continent::Continent::counter = 0;
+
+//Constructor of continent containing a list of territories from connected graph of territories
+Continent::Continent(Graph<int> *graph, vector<Territory *> *territories) {
+    *id = ++counter;
+    territoriesGraph = graph;
+    territoriesVector = territories;
 }
 
+Continent::Continent(const Continent &continent) {
+    *id = ++counter;
+    territoriesGraph = continent.territoriesGraph;
+    territoriesVector = continent.territoriesVector;
 
+} //End of copy constructor
+
+Continent::~Continent() {
+    delete id;
+    delete territoriesGraph;
+    delete territoriesVector;
+    --counter;
+} //End of destructor
+
+Continent &Continent::operator=(const Continent &continent) {
+    if (this == &continent) {
+        return *this;
+    }
+    delete territoriesVector;
+    delete territoriesGraph;
+    territoriesGraph = continent.territoriesGraph;
+    territoriesVector = continent.territoriesVector;
+    id = continent.id;
+    return *this;
+} //End of assignment operator
+
+//Check if all territories are connected
+bool Continent::Continent::isConnected() const {
+    auto *visited = new vector<int>;
+    bool isConnected = true;
+
+    // For every vertex
+    for (int vertex : territoriesGraph->get_vertices()) {
+
+        // Clear visited vector
+        visited->clear();
+
+        // Traverse that vertex
+        traverse(vertex, territoriesGraph, visited);
+
+        // If not all vertices were visited
+        if (visited->size() != territoriesGraph->get_vertices().size()) {
+            isConnected = false;
+            break;
+        }
+    }
+    delete visited;
+
+    return isConnected;
+}
+
+//Visiting each vertex
+void Continent::Continent::traverse(const int node, const Graph<int> *graph, vector<int> *visited) const {
+    // Mark node as visited
+    visited->push_back(node);
+
+    // For every neighbour of node
+    for (int neighbour : graph->get_neighbours(node)) {
+        // If neighbour of node isn't visited
+        if (!(std::find(visited->begin(), visited->end(), neighbour) != visited->end())) {
+            // Traverse into neighbour
+            traverse(neighbour, graph, visited);
+        }
+    }
+}
+
+const vector<Territory *> *Continent::Continent::getTerritoriesVector() {
+    return territoriesVector;
+}
+
+Graph<int> *Continent::Continent::getTerritoriesGraph() const {
+    return territoriesGraph;
+}
+
+int Continent::Continent::getId() const {
+    return *id;
+}
+
+ostream &operator<<(ostream &output, Continent &continent) {
+    output << "Here is Continent " << continent.getId() << " and its territories: ";
+    auto *territories = new vector<Territory *>;
+    *territories = *continent.getTerritoriesVector();
+    for (Territory *territory: *territories) {
+        output << "Territory " << territory->getId() << endl;
+    }
+    return output;
+} //End of insertion operator
+
+Map::Map(vector<Graph<int> *> *graph, vector<Continent *> *continentsVector) {
+    territoriesGraph = graph;
+    continents = continentsVector;
+}
+
+Map::Map(const Map &map) {
+    continents = map.continents;
+    territoriesGraph = map.territoriesGraph;
+} //End of Copy Constructor
+
+Map::~Map() {
+    delete continents;
+    delete territoriesGraph;
+}//End of destructor
+
+Map &Map::operator=(const Map &map) {
+    if (this == &map) {
+        return *this;
+    }
+    delete continents;
+    delete territoriesGraph;
+    continents = map.continents;
+    territoriesGraph = map.territoriesGraph;
+    return *this;
+} //End of assignment operator
+
+// Check if all territories are connected
+bool Map::Map::isConnectedGraph() const {
+    bool isConnectedGraph = true;
+    //Check every graph
+    for (Graph<int> *graph: *territoriesGraph) {
+        auto *visited = new vector<int>;
+        // For every vertex
+        for (int vertex : graph->get_vertices()) {
+
+            // Clear visited vector
+            visited->clear();
+
+            // Traverse that vertex
+            traverse(vertex, graph, visited);
+
+            // If not all vertices were visited
+            if (visited->size() != graph->get_vertices().size()) {
+                isConnectedGraph = false;
+                break;
+            }
+        }
+        delete visited;
+    }
+    return isConnectedGraph;
+}
+
+//Visiting each vertex
+void Map::Map::traverse(const int node, const Graph<int> *graph, vector<int> *visited) const {
+    // Mark node as visited
+    visited->push_back(node);
+
+    // For every neighbour of node
+    for (int neighbour : graph->get_neighbours(node)) {
+        // If neighbour of node isn't visited
+        if (!(std::find(visited->begin(), visited->end(), neighbour) != visited->end())) {
+            // Traverse into neighbour
+            traverse(neighbour, graph, visited);
+        }
+    }
+}
+
+bool Map::Map::validate() const {
+    vector<Continent *> *continentsVector = this->continents;
+    // Check if every territory in a continent is connected by choosing a starting vertex and
+    // visiting recursively every connected vertex and when it is done, verify all vertex are
+    // visited otherwise print "Unconnected continent" error
+    for (Continent *continent : *continentsVector) {
+        if (!(continent->isConnected())) {
+            cout << "Invalid: A continent is not a connected subgraph." << endl;
+            return false;
+        }
+    }
+    // Check if map is a connected graph
+    if (!(this->isConnectedGraph())) {
+        cout << "Invalid: Map is not a connected graph." << endl;
+        return false;
+    }
+    // Check if every territory belong to a single continent by making sure there are no duplicates
+    // when going through every single territory of every single continent
+    // Also check if there are territories that are in the territories graph, but not in the
+    // territories vector by comparing the size of the accumulated territories in both containers
+    // otherwise print "Duplicate territory in multiple continents" error
+    auto *territoryIds = new vector<int>;
+    int numOfTerritoriesGraph = 0;
+    int numOfTerritoriesVector = 0;
+    for (Continent *continent : *continentsVector) {
+        numOfTerritoriesVector += continent->getTerritoriesVector()->size();
+        numOfTerritoriesGraph += continent->getTerritoriesGraph()->get_n_vertices();
+        for (Territory *territory : *(continent->getTerritoriesVector())) {
+            int id = territory->getId();
+            if (std::find(territoryIds->begin(), territoryIds->end(), id) != territoryIds->end()) {
+                cout << "Invalid: Duplicate territory in multiple continents." << endl;
+                return false;
+            } else {
+                territoryIds->push_back(id);
+            }
+        }
+    }
+    //Check if number of territories in graph == number of territories in vector
+    //Otherwise print "Inconsistent number" error
+    if (numOfTerritoriesGraph != numOfTerritoriesVector) {
+        cout << "Invalid: Inconsistent number of territories in graph and in vector";
+        return false;
+    }
+    // Cleanup
+    territoryIds->clear();
+    delete territoryIds;
+    return true; //Map is validated
+}
+
+vector<Continent *> *Map::Map::getContinents() const {
+    return continents;
+}
+
+vector<Territory *> *Map::Map::getTerritories() const {
+    auto *territories = new vector<Territory *>;
+    for (Continent *continent : *continents) {
+        for (Territory *territory : *(continent->getTerritoriesVector())) {
+            territories->push_back(territory);
+        }
+    }
+    return territories;
+}
+
+ostream &operator<<(ostream &output, const Map &map) {
+    output << "The map is composed of " << map.getContinents()->size() << " continents and "
+           << map.getTerritories()->size() << "." << endl << "Here are all the list of continents and territories: "
+           << endl;
+    for (Continent *continent : *map.getContinents()) {
+        cout << continent;
+    }
+    return output;
+} //End of insertion operator
--- Part 1/Map.h
+++ Part 1/Map.h
@@ -4,43 +4,99 @@
 
 #include <vector>
 #include <string>
-#include "../Part 4/Player.h"
 #include <map>
+#include "../Part 4/Player.h"
+#include "../Part 1/Graph.h"
 
 using namespace std;
+using namespace graph;
+
+
+class Army {
+private:
+    static int counter;
+    int *id = new int(0);
+    Player *owner;
+
+public:
+    explicit Army(Player *); //Army constructor for player
+    Army(const Army &); //Copy constructor
+    ~Army(); //Destructor
+    Army &operator=(const Army &); //Assignment Operator
+
+    int getId() const;
+
+    friend ostream &operator<<(ostream &, const Army &);
+}; //end of Army class
+
+class Territory {
+private:
+    static int counter;
+    int *id = new int(0);
+    Player *owner;
+    vector<Army *> *armies;
+
+public:
+    Territory(); // Default Territory constructor with no armies present
+    explicit Territory(Player *); // Constructor for 1 player starting with 5 armies
+    Territory(const Territory &); //Copy Constructor
+    ~Territory(); //Destructor
+    Territory &operator=(const Territory &); //Assignment Operator
+    int getId() const;
+
+    friend ostream &operator<<(ostream &, const Territory &); //insertion operator
+}; //end of Territory Class
+
+class Continent {
+private:
+    static int counter;
+    int *id = new int(0);
+    vector<Territory *> *territoriesVector;
+    Graph<int> *territoriesGraph;
+
+    void traverse(int, const Graph<int> *, vector<int> *) const; //visiting each vertex of graph
+
+public:
+    Continent(Graph<int> *, vector<Territory *> *); //Constructor with graph and list of territories
+    Continent(const Continent &); //Copy Constructor
+    ~Continent(); //Destructor
+    Continent &operator=(const Continent &); //Assignment Operator
+
+    bool isConnected() const; //return true if all territories are connected in continent
+
+    const vector<Territory *> *getTerritoriesVector();
+
+    Graph<int> *getTerritoriesGraph() const;
+
+    int getId() const;
+
+    friend ostream &operator<<(ostream &, const Continent &); //insertion operator
+}; //End of Continent Class
 
 class Map {
+private:
+    vector<Continent *> *continents;
+    vector<Graph<int> *> *territoriesGraph;
+
+    void traverse(int, const Graph<int> *, vector<int> *) const; //Visiting each vertex of graph
+
 public:
-    class Army {
-    private:
-        int *id = new int(0);
-        Player *owner;
+    Map(vector<Graph<int> *> *, vector<Continent *> *);
 
-    public:
-        Army(Player *); //Army constructor for player
+    Map(const Map &); //Copy constructor
+    ~Map(); //Destructor
+    Map &operator=(const Map &); //Assignment Operator
+    bool isConnectedGraph() const;
 
-        int getId() const;
+    bool validate() const; //checking if map is a connected graph, continent is a subgraph
+    // and if each territory belongs to one and only one continent
 
-        void print() const;
-    };
+    vector<Continent *> *getContinents() const;
 
-    class Territory {
-    private:
-        static int counter;
-        int *id = new int(0);
-        Player *owner;
-        vector<Army *> *armies;
+    vector<Territory *> *getTerritories() const;
 
-    public:
-        Territory(); // Default Territory constructor with no armies present
-        Territory(Player *); // Constructor for 1 player starting with 5 armies
-
-        int getId() const;
-
-        void print() const;
-    };
-
-};
+    friend ostream &operator<<(ostream &, const Map &);
+}; //End of Map Class
 
 
 #endif //A1_MAP_H
