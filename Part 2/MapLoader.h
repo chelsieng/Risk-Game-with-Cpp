c30fcaceb9dd9e64fd09f86906b06dc3bfe3e3ef
Raffi Bezirjian <55158968+raffialan@users.noreply.github.com>
2020-10-11 12:36:33 -0400

    Update MapLoader.h

--- Part 2/MapLoader.h
+++ Part 2/MapLoader.h
@@ -1,11 +1,27 @@
+//
+// Created by Raffi Alan Bezirjian on 2020-10-01.
+//
+
 #pragma once
-#ifndef A1_MAPLOADER_H
-#define A1_MAPLOADER_H
+#include "Map.h"
+#include <string>
 
 
 class MapLoader {
 
+private:
+    string* fileName;
+    Map* resultMap;
+
+public:
+    MapLoader();                                        //Default constructor
+    MapLoader(MapLoader& map);                          //Copy constructor
+    MapLoader& operator = (const MapLoader& map);       //Assignment operator
+    ~MapLoader();                                       //Destructor
+
+    MapLoader(string fileName);
+    void ReadMap(string fileName);
+    Map* GetMap(std::string filePath);
+
+    vector<string> SplitWords(string s);
 };
-
-
-#endif //A1_MAPLOADER_H
