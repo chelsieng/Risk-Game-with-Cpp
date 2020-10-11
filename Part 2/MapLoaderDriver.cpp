1d42f6d3d32a164706f0633c6e6a6b8d91af8833
Raffi Bezirjian <55158968+raffialan@users.noreply.github.com>
2020-10-11 12:43:06 -0400

    Update MapLoaderDriver.cpp

--- Part 2/MapLoaderDriver.cpp
+++ Part 2/MapLoaderDriver.cpp
@@ -1,1 +1,23 @@
+//
+// Created by Raffi Alan Bezirjian on 2020-10-10.
+//
 
+#include "MapLoader.h"
+#include <iostream>
+#include <string>
+
+// testing the .map files
+
+ int main() {
+	MapLoader* mapLoader = new MapLoader();
+	Map* map = mapLoader->GetMap("arctic.map");  // we can try other .map files
+    std::cout << endl;
+
+//  Testing a failed .map file (invalid.map)
+   
+// 	*MapLoader* mapLoaderInvalid = new MapLoader();
+// 	mapLoaderFail->ReadMap("invalid.map");
+// 	std::cout << endl;*/
+
+
+ }
