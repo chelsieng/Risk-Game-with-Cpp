10b4fa7bfcd2e1daca495a696b0d325aeb0c2f42
chelsieng <chelsiengmk@gmail.com>
2020-10-12 13:41:24 -0400

    Modified .cpp file

--- Part 2/MapLoader.cpp
+++ Part 2/MapLoader.cpp
@@ -1,19 +1,17 @@
 #include "MapLoader.h"
 
-// Constructor that reads a domination to check whethere file is valid or not,
-// If file is valid, create Map object
-// If file is invalid, print "Invalid file" error
+// Constructor that reads a domination map file to create map object
 MapLoader::MapLoader(string fileName) {
 
 }
 
 void MapLoader::copy(const MapLoader &mapLoader) {
-    fileName = new string (mapLoader.fileName);
+    fileName = new string(*mapLoader.fileName);
 }
 
 MapLoader::MapLoader(MapLoader &mapLoader) {
-
-} //Copy Construtor
+    fileName = new string(*mapLoader.fileName);
+} //Copy Constructor
 
 MapLoader &MapLoader::operator=(const MapLoader &mapLoader) {
     //handling self assignment
