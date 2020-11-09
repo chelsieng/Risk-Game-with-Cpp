83d32317327afc92073318100c00da4c3b14f9c5
chelsieng <chelsiengmk@gmail.com>
2020-11-08 23:32:45 -0500

    Added Map Files directory outside + included invalid map

--- Part 2/MapLoaderDriver.cpp
+++ Part 2/MapLoaderDriver.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main() {
     // Change path name to any files in Map Files directory
-    auto *a = new MapLoader("../Part 2/Map Files/test.txt");
+    auto *a = new MapLoader("../Map Files/berlin.map");
     cout << *a->getMap() << endl;
     if (a->getMap()->validate()) {
         cout << "Pass: Map is valid" << endl;
