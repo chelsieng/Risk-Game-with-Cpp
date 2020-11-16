3b62a3621d5617c1134b004f2e5645b5d5f10455
chelsieng <chelsiengmk@gmail.com>
2020-11-16 18:03:44 -0500

    Edited insertion operator

--- Part 4/Player.cpp
+++ Part 4/Player.cpp
@@ -13,11 +13,10 @@ Player::Player() :
         playerOrdersList{nullptr},
         playerTerritories{nullptr},
         pID{++counter}, // increasing player count
-        reinforcementPool{0}
-{}
+        reinforcementPool{0} {}
 
 // parameterized constructor
-Player::Player(Hand* hand, OrdersList* OrdersList, vector<Territory*>* territories) {
+Player::Player(Hand *hand, OrdersList *OrdersList, vector<Territory *> *territories) {
     pID = ++counter; // increasing player count
     this->playerHand = hand;
     this->playerOrdersList = OrdersList;
@@ -47,7 +46,7 @@ Player::Player(const Player &playerCopy) {
 
     // TERRITORIES
     // deallocate
-    for (Territory* t : *this->playerTerritories) {
+    for (Territory *t : *this->playerTerritories) {
         delete t;
         t = nullptr;
     }
@@ -75,7 +74,7 @@ Player::~Player() {
     playerOrdersList = nullptr;
 
     // free territories
-    for (Territory* t : *playerTerritories) {
+    for (Territory *t : *playerTerritories) {
         delete t;
         t = nullptr;
     }
@@ -89,37 +88,37 @@ int Player::getId() const {
 }
 
 // Returning the player's order list
-OrdersList* Player::getOrdersList() {
+OrdersList *Player::getOrdersList() {
     return this->playerOrdersList;
 }
 
 // Returning the player's hand
-Hand* Player::getHand() {
+Hand *Player::getHand() {
     return this->playerHand;
 }
 
-vector<Territory*>* Player::getPlayerTerritories(){
+vector<Territory *> *Player::getPlayerTerritories() {
     return this->playerTerritories;
 }
 
 // Adding an order to the end of the player's order list
 // A subclass of Order (order types) will be passed as a parameter.
-void Player::issueOrder(Order* orderToIssue) {
-    Order* newOrder = orderToIssue;
+void Player::issueOrder(Order *orderToIssue) {
+    Order *newOrder = orderToIssue;
     this->playerOrdersList->addToLast(newOrder);
 }
 
-vector<Territory*>* Player::toDefend(vector<Territory*>* listToDefend) {
+vector<Territory *> *Player::toDefend(vector<Territory *> *listToDefend) {
     // Returning territories to defend
     return listToDefend;
 }
 
-std::vector<Territory*>* Player::toAttack(vector<Territory*>* listToAttack) {
+std::vector<Territory *> *Player::toAttack(vector<Territory *> *listToAttack) {
     // Returning territories to defend
     return listToAttack;
 }
 
-Player &Player::operator=(const Player& player) {
+Player &Player::operator=(const Player &player) {
 
     // Checking for self assignment
     if (this == &player) {
@@ -135,7 +134,7 @@ Player &Player::operator=(const Player& player) {
     delete playerOrdersList;
     playerOrdersList = nullptr;
 
-    for (Territory* t : *playerTerritories) {
+    for (Territory *t : *playerTerritories) {
         delete t;
         t = nullptr;
     }
@@ -145,21 +144,25 @@ Player &Player::operator=(const Player& player) {
     // creating a deep copy
     playerHand = new Hand(*player.playerHand);
     playerOrdersList = new OrdersList(*player.playerOrdersList);
-    playerTerritories = new vector<Territory*>(*player.playerTerritories);
+    playerTerritories = new vector<Territory *>(*player.playerTerritories);
 
     return *this;
 }
 
-std::ostream &operator<<(ostream& out, const Player& player) {
-    out << "PLAYER " << player.getId() << " HAND : " << endl;
+std::ostream &operator<<(ostream &out, const Player &player) {
+    out << "PLAYER " << player.getId() << " HAND: " << endl;
     out << *player.playerHand << endl;
-    out << "PLAYER " << player.getId() <<  " TERRITORIES : " << endl;
-    for (auto terr : *player.playerTerritories) {
-        out << *terr << endl;
+    out << "PLAYER " << player.getId() << " TERRITORIES: " << endl;
+    if (player.playerTerritories == nullptr) {
+        out << "Territories will be assigned later." << endl <<endl;
+    } else {
+        for (auto terr : *player.playerTerritories) {
+            out << *terr << endl;
+        }
     }
-    out << "PLAYER " << player.getId() << " ORDERS : " << endl;
+    out << "PLAYER " << player.getId() << " ORDERS: " << endl;
     out << *player.playerOrdersList << endl;
-    out << "PLAYER " << player.getId() << " AVAILABLE (UNUSED) REINFORCEMENT : " << endl;
+    out << "PLAYER " << player.getId() << " AVAILABLE (UNUSED) REINFORCEMENT: " << endl;
     out << player.reinforcementPool << endl;
 
     return out;
